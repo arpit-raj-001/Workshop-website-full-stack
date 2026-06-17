76345270ebe14a9cfacd7b0d41b734b90944bdfd
Arpit <arpit1206477417@gmail.com>
2026-06-17 22:07:17 +0530

    multer setup for upload

---
+++ backend/middleware/upload.js
@@ -0,0 +1,31 @@
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // front end form me se ek field name le lena file ke liye
+    if (file.fieldname === "photo") {
+      cb(null, "uploads/photos/");
+    } else if (file.fieldname === "video") {
+      cb(null, "uploads/videos/");
+    } else {
+      cb(null, "uploads/");
+    }
+  },
+
+  filename: (req, file, cb) => {
+    const randomHex = crypto.randomBytes(8).toString("hex"); //random 8letter string
+    const extension = path.extname(file.originalname);
+    cb(null, randomHex + extension);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 300 * 1024 * 1024, // abhi ke liye 300mb rkha he
+  },
+});
+
+module.exports = upload;
