a0f13174fd3cf43dc851d397667197e05bbf079e
Arpit <arpit1206477417@gmail.com>
2026-06-17 21:02:16 +0530

    passport handling

---
+++ backend/config/passport.js
@@ -0,0 +1,48 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const { User } = require("../models");
+require("dotenv").config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    //google will give name , email , profile etc.
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const email = profile.emails[0].value;
+        let user = await User.findOne({ where: { googleId: profile.id } }); //check if already exist
+
+        const adminEmails = process.env.ADMIN_EMAILS
+          ? process.env.ADMIN_EMAILS.split(",")
+          : [];
+        const role = adminEmails.includes(email) ? "admin" : "user"; //agar admin email me daala ho to admin bana do
+
+        //new user handling
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: email,
+            avatar: profile.photos[0] ? profile.photos[0].value : null,
+            role: role,
+          });
+        } else {
+          //puraane user handling by updating their role
+
+          user.role = role;
+          await user.save();
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error, null);
+      }
+    },
+  ),
+);
+
+module.exports = passport;
